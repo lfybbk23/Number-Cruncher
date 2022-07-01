8319d6304ec0e4bcb7b42808b23a46a9955c3dec
Daniil <lfybbk774@gmail.com>
2022-07-01 16:00:12 +0300

    Quadratic equation finder.

---
+++ QuadraticEquation.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+using namespace std;
+
+int main() {
+	float a, b, c, x1, x2, discriminant, real, imaginary;
+	cout << "Enter elements a, b and c: ";
+	cin >> a >> b >> c;
+	discriminant = b * b - 4 * a * c;
+
+	if (discriminant > 0) {
+		x1 = (-b + sqrt(discriminant)) / (2 * a);
+		x2 = (-b - sqrt(discriminant)) / (2 * a);
+		cout << "Roots are real and different." << endl;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
+	}
+
+	else if (discriminant == 0) {
+		cout << "Roots are real and same." << endl;
+		x1 = -b / (2 * a);
+		cout << "x1 = x2 =" << x1 << endl;
+	}
+
+	else {
+		real = -b / (2 * a);
+		imaginary = sqrt(-discriminant) / (2 * a);
+		cout << "Roots are hard to determine and different." << endl;
+		cout << "x1 = " << real << "+" << imaginary << "i" << endl;
+		cout << "x2 = " << real << "-" << imaginary << "i" << endl;
+	}
+
+	return 0;
+}
