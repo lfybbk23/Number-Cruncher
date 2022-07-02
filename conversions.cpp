5f162b8efddcac496d9381a451f1357d724cc5db
Daniil <lfybbk774@gmail.com>
2022-07-02 13:41:39 +0300

    Meter to Killometer

---
+++ conversions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<conio.h>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+	float m;
+	cout << "Enter meter: " << endl;
+	cin >> m;
+	float km = m / 1000;
+	cout << "Killometer = " << km;
+}
